ce659e1b65de6907885cb8473274d4323e66f77d
BogdanNetan <netan.bogdan@gmail.com>
2020-02-12 20:23:24 +0200

    sending create task request to api

--- js/tasks.js
+++ js/tasks.js
@@ -15,6 +15,27 @@ $.ajax ({
 
     },
 
+    createTask: function () {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
+
+        let requestBody = {
+            description:descriptionValue,
+            dealine:deadlineValue
+        } ;
+
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "POST",
+            // also known as mime type
+            contentType: "application/json",
+            data: Json.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getTasks();
+
+        });
+    },
+
     getTaskRow:function (task) {
         // spread operator(...)
         let formattedDeadline = new Date(...task.deadline).toLocaleDateString("ro");
@@ -32,7 +53,7 @@ $.ajax ({
 
         return ` <tr>
         <td>${task.description}</td>
-        <td>1${formattedDeadline}</td>
+        <td>${formattedDeadline}</td>
         <td><input type="checkbox" data-id= ${task.id} class="mark-done" ${checkedAtribute}/></td>
         <td><a href="#" data-id=${task.id} class="delete-task">X</a> </td>
 
@@ -46,8 +67,19 @@ $.ajax ({
         tasks.forEach(task => tableBody += ToDoList.getTaskRow(task));
 
         $("#tasks-table tbody").html(tableBody);
+    },
+    bindEvents: function () {
+        // capturing the 'submit form' event to bind our function to it
+        $("#new-task-form").submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createTask();
+
+        })
+
     }
 
 };
 
 ToDoList.getTasks();
+ToDoList.bindEvents();
